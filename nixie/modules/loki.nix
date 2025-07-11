aaf906fc39a3848a0548ecd76ca31a7b20e31e19
quantinium03 <quant@quantinium.dev>
2025-07-11 18:01:38 +0530

    new config

--- nixie/modules/loki.nix
+++ nixie/modules/loki.nix
@@ -4,71 +4,52 @@
     configuration = {
       server = {
         http_listen_port = 8500;
-        grpc_listen_port = 9096;
-        log_level = "debug";
-        grpc_server_max_concurrent_streams = 1000;
       };
       auth_enabled = false;
 
-      common = {
-        instance_addr = "127.0.0.1";
-        path_prefix = "/tmp/loki";
-        storage = {
-          filesystems = {
-            chunks_directory = "/tmp/loki/chunks";
-            rules_directory = "/tmp/loki/rules";
+      ingestor = {
+        lifecycler = {
+          address = "127.0.0.1";
+          ring = {
+            kvstore.store = "inmemory";
+            replication_factor = 1;
           };
+          final_step = "0s";
         };
-        replication_factor = 1;
-        ring = {
-          kvstore = {
-            store = "inmemory";
-          };
-        };
+        # chunk not receiving logs in 1hr will be flushed
+        chunk_idle_period = "1h";
+        # all chunk with this age will be flushed
+        max_chunk_age = "1h";
+
+        chunk_target_size = "1048576"; # 1.5mb
+        chunk_retain_period = "30s";
       };
 
-      query_range = {
-        results_cache = {
-          cache = {
-            embedded_cache = {
-              enabled = true;
-              max_size_mb = 100;
-            };
+      schema_config.configs = [
+        {
+          from = "2020-05-15";
+          store = "tsdb";
+          object_store = "filesystem";
+          schema = "v13";
+          index = {
+            prefix = "index_";
+            period = "24h";
           };
-        };
+        }
+      ];
+
+      storage_config = {
+        filesystem.directory = "/tmp/loki/chunks";
       };
 
       limits_config = {
-          metric_aggregation_enabled = true;
-          enable_multi_variant_queries = true;
-          reject_old_samples = true;
-          reject_old_samples_max_age = "168h";
+        reject_old_sample = true;
+        reject_old_samples_max_age = "168h";
       };
 
-      schema_config = {
-        config = [
-          { 
-              from = "2025-07-11";
-              store = "tsdb";
-              object_store = "filesystem";
-              schema = "v13";
-              index = {
-                  prefix = "index_";
-                  period = "24h";
-              };
-          }
-        ];
-      };
-    
       table_manager = {
-          retention_deleted_enabled = false;
-          retention_period = "0s";
-      };
-
-      storage_config = {
-          filesystem = {
-              directory = "/tmp/loki/chunks";
-          };
+        retention_deletes_enabled = false;
+        retention_period = "0s";
       };
     };
   };
