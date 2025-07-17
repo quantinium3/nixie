fdbd3874cbce02ccb0511ffac4cde7c3c1cacef4
quantinium03 <quant@quantinium.dev>
2025-07-17 17:01:51 +0530

    idk man

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
